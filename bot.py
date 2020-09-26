7f8183e546f3df455a9e399ee3ae335343ae2425
Jenna Chadwick <70163429+JennaChadwick@users.noreply.github.com>
2020-09-26 13:57:40 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,8 @@ client= commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Bot is ready. ')
 
+@client.command()
+async def respond(ctx):
+    await ctx.send('hello')
+
 client.run('NzU5NDYyMTM0NTc1NzkyMTI5.X292Ow.tNm-GHcYe5WOyh751_yisfL5B6M')
