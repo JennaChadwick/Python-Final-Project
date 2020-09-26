782c46fd4975c7cea74b91321f40fc741cd19fdb
Jenna Chadwick <70163429+JennaChadwick@users.noreply.github.com>
2020-09-26 13:39:40 -0600

    Add files via upload
    Added the base bot file

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+client= commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready. ')
+
+client.run('NzU5NDYyMTM0NTc1NzkyMTI5.X292Ow.tNm-GHcYe5WOyh751_yisfL5B6M')
