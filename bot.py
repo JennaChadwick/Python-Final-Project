1a52d33802ef7a66d208f46bafadae837d90e8bc
Jenna Chadwick <70163429+JennaChadwick@users.noreply.github.com>
2020-10-31 14:22:16 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ load_dotenv('.env')
 
 def get_some_text():
     f = open(testText.txt, "r")
+    f.close()
     return f
 
 client= commands.Bot(command_prefix = '.')
@@ -21,5 +22,9 @@ async def on_ready():
 @client.command()
 async def respond(ctx):
     await ctx.send('hello number 2')
+    
+@client.command()
+async def test(ctx):
+    await ctx.send('test')
 
 client.run(os.getenv('TUTORIAL_BOT_TOKEN'))
