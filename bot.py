c6884871d8cbefbfd168a203e78b480af30ec6b7
Jenna Chadwick <70163429+JennaChadwick@users.noreply.github.com>
2020-10-31 12:37:37 -0600

    Update bot.py
    Testing text reading

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
 
+def get_some_text():
+    f = open(testText.txt, "r")
+    return f
+
 client= commands.Bot(command_prefix = '.')
 
 @client.event
@@ -9,6 +13,6 @@ async def on_ready():
 
 @client.command()
 async def respond(ctx):
-    await ctx.send('hello')
+    await ctx.send(get_some_text())
 
 #client.run('NzU5NDYyMTM0NTc1NzkyMTI5.X292Ow.Q-4KaGUkST6Q_0S9xsw5o0WppYg')
