5a3917876f5ba26efe2656d3ea44576ada86eaa4
anguyen7-99 <60800455+anguyen7-99@users.noreply.github.com>
2020-11-07 13:31:01 -0700

    Added user name

--- bot.py
+++ bot.py
@@ -34,25 +34,38 @@ def read_user_profile(username):
 
 client= commands.Bot(command_prefix = '.')
 
+# To turn on the Discord Bot
 @client.event
 async def on_ready():
     print('Bot is ready. ')
 
+# To enter .respond command for bot to respond to USER
 @client.command()
 async def respond(ctx):
     await ctx.send('Never gonna give you up\nNever gonna let you donw\nNever gonna run around\nAnd hurt you')
 
+# To enter a .test command for bot to write to load a text file
 @client.command()
 async def test(ctx):
     await ctx.send(get_some_text())
 
+# To enter .testWrite command for bot to write to a text file
 @client.command()
 async def testWrite(ctx):
     write_some_text()
 
 
+
 @client.command()
 async def userRead(ctx):
     await ctx.send(read_user_profile('Jeb'))
+
+
+# To enter .userName command for bot to grab a Discord username
+@client.command()
+async def userName(ctx):
+    await ctx.send()
+
+# Discord Bot token
 
 client.run(os.getenv('TUTORIAL_BOT_TOKEN'))
