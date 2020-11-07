d17066af0998ee50beb09118fdb08f23303afaf3
JennaChadwick <chadwickjennak@gmail.com>
2020-11-07 13:16:57 -0700

    First pass at userInfo file reading

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 # Imports
 from dotenv import load_dotenv
 import os
+import re
 
 # Credentials
 load_dotenv('.env')
@@ -18,6 +19,18 @@ def write_some_text():
     f = open('testText.txt', "a")
     f.write("\nExtra Line")
     f.close()
+
+
+def read_user_profile(username):
+    f = open('userInfo.txt', "r")
+    text = f.readlines()
+    f.close()
+    userReg = re.compile(username)
+    for l in text:
+        if userReg.search(l):
+            return l
+    return 'No User Specified'
+
 
 client= commands.Bot(command_prefix = '.')
 
@@ -36,5 +49,10 @@ async def test(ctx):
 @client.command()
 async def testWrite(ctx):
     write_some_text()
+
+
+@client.command()
+async def testWrite(ctx):
+    await ctx.send(read_user_profile(Jeb))
 
 client.run(os.getenv('TUTORIAL_BOT_TOKEN'))
